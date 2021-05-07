3dc73656ee870a983684f9df046666372a65d0ca
saebom <boma91@gmail.com>
2021-05-08 08:16:45 +0900

    Add: home css 수정

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,14 +4,14 @@ import './Home.scss';
 const Home = () => {
     return (
         <div className="home">
+            <p>1111</p>
             <p>sdfsdfd</p>
             <p>sdfsdfd</p>
             <p>sdfsdfd</p>
             <p>sdfsdfd</p>
             <p>sdfsdfd</p>
             <p>sdfsdfd</p>
-            <p>sdfsdfd</p>
-            
+
             
         </div>
     );
