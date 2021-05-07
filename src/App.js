b23d26dbba085ad8736595e15cf1a88e1b65d7ad
saebom <boma91@gmail.com>
2021-05-08 04:26:18 +0900

    Add: nav UI 생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from 'react'
-import './App.css';
+import { Link, Route, Switch } from 'react-router-dom';
+import './App.scss';
 
 function App() {
+
+  function handleNav() {
+    let nav = document.querySelector('#nav');
+    nav.classList.toggle('active')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <nav className="nav_container">
+        <div className="active" id="nav">
+          <ul>
+              <Link className="li" to="/"> Home </Link>
+              <Link className="li" to="/"> Introduce </Link>
+              <Link className="li" to="/"> Skills </Link>
+              <Link className="li" to="/"> Portfolio </Link>
+              <Link className="li" to="/"> Contact </Link>
+          </ul>
+          <button className="icon" onClick={handleNav}>
+              <div className="line line1"></div>
+              <div className="line line2"></div>
+          </button>
+          </div>
+      </nav>
 
-      </header>
+      <Switch>
+
+        <Route>
+          dkssudgktkpdy
+           sdfsdfdsfsd
+          sdfsdfdsfsd
+        </Route>
+
+        <Route>
+        </Route>
+
+        <Route>
+        </Route>
+
+      </Switch>
     </div>
   );
 }
