bb4ffba65b2e036bd0986815e99326d0ba71d4d5
saebom <boma91@gmail.com>
2021-05-09 10:24:46 +0900

    Add: introduce info콘텐츠 추가

--- src/Components/Introduce/Introduce.js
+++ src/Components/Introduce/Introduce.js
@@ -9,20 +9,16 @@ const Introduce = () => {
 
                 <div className="info">
                     <div>
-                        <i class="fas fa-birthday-cake"></i>
+                        <i class="fas fa-user-alt"></i>
                         <p>이 새 봄</p>
                     </div>
                     <div>
-                        <i class="fas fa-phone-alt"></i>
+                        <i class="fas fa-birthday-cake"></i>
                         <p>91. 05. 02</p>
                     </div>
                     <div>
-                        <i class="fas fa-envelope"></i>
-                        <p>boma91@gmail.com</p>
-                    </div>
-                    <div>
                         <i class="fas fa-graduation-cap"></i>
-                        <div className="graduate">
+                        <div className="text">
                             <p>덕성여자대학교</p>
                             <p className="major">유아교육학과 전공/ 영어영문학과 복수전공</p>
                         </div>
@@ -30,6 +26,16 @@ const Introduce = () => {
                     <div className="last">
                         <i class="fas fa-building"></i>
                         <p>마케팅 기획자(AE)로 2년 근무</p>
+                    </div>
+
+                    <div>
+                        <i class="fas fa-key"></i>
+                        <div  className="text">
+                            <p className="motto">트렌디한 UI와 인터랙티브한 UX를 최우선으로 생각하며,</p>
+                            <p className="motto"><strong>가독성</strong>과 <strong>재활용성</strong> 높은 코드 구현을 지향합니다.</p>
+                            <p className="motto lower">React, JavaScript, HTML5, CSS3 활용 역량을 키워왔고,</p> 
+                            <p className="motto"><strong>최적의 사용자 경험 제공</strong>을 목표로 하여 지속적으로 발전해나가고 싶습니다.</p>
+                        </div>
                     </div>
                 </div>
             </div>
