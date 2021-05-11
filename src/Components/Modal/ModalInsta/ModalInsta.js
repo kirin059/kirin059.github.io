9b339bc2dcd88034745a2ac485d21b2d7da1a593
saebom <boma91@gmail.com>
2021-05-12 06:35:50 +0900

    Add: project modal 내용 layout 수정

--- src/Components/Modal/ModalInsta/ModalInsta.js
+++ src/Components/Modal/ModalInsta/ModalInsta.js
@@ -23,21 +23,31 @@ const ModalInsta = (props) => {
                             <div className="imgs second"></div>
                         </div>
                         <div className="script">
-                            <div>Shoes 쇼핑몰을 컨셉으로 작업한 페이지 입니다. 페이지는 총5개입니다. </div>
-                            <div>참여도: 100%</div>
-                            <div>사용 툴: react, redux, sass, bootstrap, axios</div>
+                            <div style={{'margin-bottom':'30px'}}>React 함수형 컴포넌트와 Sass를 활용하여 Instagram Clone 제작 및 Netlify로 직접 웹사이트 배포한 프로젝트입니다. </div>
+                            <div style={{ 'margin-bottom': '10px' }}>
+                                <i class="fas fa-circle" style={{ 'color': '#D09E88', 'paddingRight': '10px' }}></i>
+                                기술스택
+                            </div>
+                            <div style={{'margin-bottom':'30px'}}>HTML5, Javascript, React, Redux, React-router-dom, Sass, axios</div>
 
-                            <div>페이지별 기능</div>
-                            <div>메인페이지: 더보기버튼을 누르면 상품이 추가적으로 나오도록 함</div>
+                            {/* <div>페이지별 기능</div>
                             <div>디테이페이지: 상품을 클릭하면 고유 상품 id에 매칭되는 상품의 상세 페이지가 보입니다. 주문하기를 누르면 cart페이지로 이동하며 상품 갯수가 추가됩니다</div>
                             <div>카트 페이지: 수량 증감 기능이 있으며 </div>
-                            <div>로그인/회원가입 페이지: validaton을 통해 유효성검사 </div>
+                            <div>로그인/회원가입 페이지: validaton을 통해 유효성검사 </div> */}
                             
-                            <div>적용 기술</div>
+                            <div style={{ 'margin-bottom': '10px' }}>
+                                <i class="fas fa-circle" style={{ 'color': '#D09E88', 'paddingRight': '10px' }}></i>
+                                구현사항
+                            </div>
                             <div>React-Router를 활용한 SPA 라우팅</div>
                             <div>reduce를 활용한 상태관리</div>
                             <div>React hook(useHistory, lestContext, useContext, useState)등을 활용한 </div>
                             <div>axios를 통한 서버 api 용청/응답 처리</div>
+
+                            <div style={{'margin-top': '30px'}}>
+                                <i class="fab fa-github" style={{ 'color': '#D09E88', 'fontSize': '40px', 'paddingRight':'20px' }}></i>
+                                <i class="fas fa-link" style={{ 'color': '#D09E88', 'fontSize': '40px' }}></i>
+                            </div>
                         </div>
                     </div>
 
