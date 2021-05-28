5d92d28f2aecb61833e92d62173c80aed884a229
saebom <boma91@gmail.com>
2021-05-28 15:30:59 +0900

    Add: home 수정

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Skills.scss';
 
 const Skills = () => {
+
     return (
         <div className='skills'>
             <div className="header">
@@ -27,7 +28,7 @@ const Skills = () => {
                     <div className="icon javascript"></div>
                     <div className="content">
                         <p className="title"><strong>Javascript</strong></p>
-                        <p>ES6 기준 Javascript를 능숙하게 다룰 수 있습니다. 크로스 브라우징이 가능한 코드를 작성할 수 있습니다.</p> 
+                        <p>ES6 기준 Javascript를 능숙히 다룰 수 있습니다. 크로스 브라우징이 가능한 코드를 작성할 수 있습니다.</p> 
                     </div>
                 </div>
                 <div className="stack_box">
@@ -41,14 +42,14 @@ const Skills = () => {
                     <div className="icon redux"></div>
                     <div className="content">
                         <p className="title"><strong>Redux</strong></p>
-                        <p>Redux 상태 관리 패턴을 적용해 본 경험이 있습니다. </p> 
+                        <p>Redux 중앙 상태 관리 패턴을 및 효율적인 모듈화를 적용해 본 경험이 있습니다. </p> 
                     </div>
                 </div>
                 <div className="stack_box">
                     <div className="icon sass"></div>
                     <div className="content">
                         <p className="title"><strong>Sass</strong></p>
-                        <p>Sass와 같은 CSS pre-processor 사용에 능숙합니다. </p> 
+                        <p>Nesting, Mixin 등을 적극 활용하여 유지보수가 용이하도록 효율적이고 가독성 좋은 코드 작성이 가능합니다.</p> 
                     </div>
                 </div>
                 <div className="stack_box">
@@ -57,7 +58,14 @@ const Skills = () => {
                         <p className="title"><strong>Styled Components</strong></p>
                         <p>Styled Components와 같은 CSS pre-processor 사용에 능숙합니다.</p>
                     </div>
-                </div>    
+                </div>
+                <div className="stack_box">
+                    <div className="icon git"></div>
+                    <div className="content">
+                        <p className="title"><strong>Git & Github</strong></p>
+                        <p>Github와 Git의 사용법을 숙지하고 있습니다. Commit관리, Merge를 통한 branch병합 등 기본적인 기능을 다룰 수 있습니다. Git, Github를 이용해 능률적인 프로젝트 협업이 가능합니다.</p>
+                    </div>
+                </div>  
             </div>
         </div>
     );
