7599b357e250872ae6a0fefc3f7ba9a0cf9f6be9
saebom <boma91@gmail.com>
2021-10-30 13:47:17 +0900

    build

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,14 @@ import './Footer.scss';
 const Footer = () => {
     return (
         <div className="footer">
-            <p>본 페이지는 상업적 목적이 아닌 개인 포트폴리오용으로 제작되었습니다.</p>
+            <div className="icon-container">
+                <i class="fab fa-github" onClick={() => {
+                    window.open('https://github.com/kirin059', '_blank')
+                }}></i>
+                <i class="fab fa-blogger" onClick={() => {
+                    window.open('https://velog.io/@kirin', '_blank')
+                }}></i>
+            </div>
             <p>  © 2021 Lee, Sae-Bom.   All Rights Reserved.</p>
         </div>
     );
